2e7e39f8ef6455896a271eb5e32a1d031b80a78b
itsqal <faiqalanaking@gmail.com>
2026-03-04 13:45:51 +0700

    Add community based filtering for input

--- src/main.py
+++ src/main.py
@@ -7,21 +7,13 @@ engine = InferenceEngine()
 
 class PredictionInput(BaseModel):
     lik_codes: list[str] = Field(..., description="Daftar kode tanda alam (contoh: ['wn-1', 'wn-3'])")
-    level_of_interaction_with_disaster: float = Field(..., ge=0, le=10, description="Skor interaksi (0-10)")
-    age: float = Field(..., ge=0, description="Usia dalam tahun")
-    usage_duration: float = Field(..., ge=0, description="Durasi penggunaan tanda alam dalam tahun")
-    min_frequency_of_usage: float = Field(..., ge=0, description="Frekuensi penggunaan tanda alam dalam sebulan")
-    fishing_experience: float = Field(..., ge=0, description="Pengalaman melaut dalam tahun")
+    beach_location: str = Field(..., description="Lokasi pantai (contoh: 'pantai_lampuuk')")
 
     class Config:
         json_schema_extra = {
             "example": {
                 "lik_codes": ["wn-1", "wn-7"],
-                "level_of_interaction_with_disaster": 2.0,   # High Risk (<= 2.50)
-                "age": 50.0,                                # High Risk (>= 47.50)
-                "usage_duration": 10.0,                     # High Risk (<= 16.50)
-                "min_frequency_of_usage": 5.0,              # High Risk (<= 6.50)
-                "fishing_experience": 15.0                  # High Risk (>= 6.50)
+                "beach_location": "pantai_lampuuk"
             }
         }
 
@@ -34,17 +26,35 @@ def predict_risk(input_data: PredictionInput):
     """
     Endpoint untuk mendapatkan analisis risiko perilaku dan validitas tanda alam.
     """
+    lik_code_list = {"lik_codes": input_data.lik_codes}
 
-    data_for_engine = {
-        "lik_codes": input_data.lik_codes,
-        "features": {
-            "level_of_interaction_with_disaster": input_data.level_of_interaction_with_disaster,
-            "age": input_data.age,
-            "usage_duration": input_data.usage_duration,
-            "frequency_of_usage": input_data.min_frequency_of_usage,
-            "fishing_experience": input_data.fishing_experience
+    if input_data.beach_location.lower() == "pantai_lampuuk":
+        features_for_engine = {
+            "features": engine.pantai_lampuuk_characteristics_input
         }
-    }
+        data_for_engine = lik_code_list | features_for_engine
+    elif input_data.beach_location.lower() == "pantai_lhoknga": 
+        features_for_engine = {
+            "features": engine.pantai_lhoknga_characteristics_input
+        }
+        data_for_engine = lik_code_list | features_for_engine
+    elif input_data.beach_location.lower() == "pantai_ulee_lheue":
+        features_for_engine = {
+            "features": engine.pantai_ulee_lheue_characteristics_input
+        }
+        data_for_engine = lik_code_list | features_for_engine
+    elif input_data.beach_location.lower() == "pantai_depok":
+        features_for_engine = {
+            "features": engine.pantai_depok_characteristics_input
+        }
+        data_for_engine = lik_code_list | features_for_engine
+    elif input_data.beach_location.lower() == "pantai_samas":
+        features_for_engine = {
+            "features": engine.pantai_samas_characteristics_input
+        }
+        data_for_engine = lik_code_list | features_for_engine
+    else: 
+        raise HTTPException(status_code=400, detail="Lokasi pantai tidak valid. Pilih salah satu: pantai_lampuuk, pantai_lhoknga, pantai_ulee_lheue, pantai_depok, pantai_samas.")
     
     try:
         result = engine.predict(data_for_engine)
